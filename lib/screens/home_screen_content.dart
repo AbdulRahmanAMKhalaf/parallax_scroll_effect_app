fc1c0d7294300e9ac9da9f8a26a0c6cf6d8cb3f3
AbdulRahman Ayman <140063652+AbdulRahmanAMKhalaf@users.noreply.github.com>
2024-07-08 01:17:45 +0300

    Initial commit

--- lib/screens/home_screen_content.dart
+++ lib/screens/home_screen_content.dart
@@ -1,8 +1,8 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:parallax_scroll_effect_app/models/home_model.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
+import 'dart:math' as math;
 
 class HomeScreenContent extends StatefulWidget {
   const HomeScreenContent({super.key});
@@ -12,8 +12,9 @@ class HomeScreenContent extends StatefulWidget {
 }
 
 class _HomeScreenContentState extends State<HomeScreenContent> {
+  late PageController pageController = PageController(viewportFraction: 0.85);
+  bool isDrag = false;
 
-  bool isDrag=false;
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -22,7 +23,117 @@ class _HomeScreenContentState extends State<HomeScreenContent> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            CarouselSlider(
+            SizedBox(
+              height: 60.h,
+              child: PageView.builder(
+                itemCount: homeList.length,
+                controller: pageController,
+                itemBuilder: (context, index) {
+                  return AnimatedBuilder(
+                    animation: pageController,
+                    builder: (context, child) {
+                      double pageOffest = 0;
+                      if(pageController.position.haveDimensions){
+                        pageOffest=pageController.page!-index-1;
+                      }
+                      double gauss =
+                      math.exp(-(math.pow((pageOffest.abs() - 0.5), 2) / 0.08));
+                      return Transform.translate(
+                        offset: Offset(-32 * gauss * pageOffest.sign, 0),
+                        child: Container(
+                          margin: EdgeInsets.all(3.w),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(
+                              20.px,
+                            ),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.vertical(top: Radius.circular(20.px)),
+                                child: Image.asset(
+                                    height: 30.h,
+                                    fit: BoxFit.cover,
+                                    alignment: Alignment(-pageOffest,0),
+                                    homeList[index].image),
+                              ),
+                              SizedBox(
+                                height: 1.h,
+                              ),
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(15.0),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        homeList[index].text,
+                                        maxLines: 2,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                            color: Colors.black87,
+                                            fontSize: 22.px,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                      SizedBox(
+                                        height: 1.h,
+                                      ),
+                                      Text(
+                                        '4 20-30',
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                            color: Colors.grey.shade400,
+                                            fontSize: 15.px,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                      const Spacer(),
+                                      Row(
+                                        children: [
+                                          ElevatedButton(
+                                            onPressed: () => () {},
+                                            style: ButtonStyle(
+                                              backgroundColor: WidgetStatePropertyAll(
+                                                  Colors.black87.withOpacity(0.8)),
+                                              minimumSize:
+                                              const WidgetStatePropertyAll(
+                                                  Size(30, 50)),
+                                            ),
+                                            child: Text(
+                                              'Reserve',
+                                              style: TextStyle(
+                                                fontSize: 20.px,
+                                                fontWeight: FontWeight.w700,
+                                                color: Colors.white70,
+                                              ),
+                                            ),
+                                          ),
+                                          const Spacer(),
+                                          Text(
+                                            '0.00\$',
+                                            style: TextStyle(
+                                                color: Colors.black87,
+                                                fontSize: 25.px,
+                                                fontWeight: FontWeight.w700),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ).animate().fadeIn(),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+            /* CarouselSlider(
                 items: List.generate(
                     homeList.length,
                     (index) {
@@ -43,6 +154,7 @@ class _HomeScreenContentState extends State<HomeScreenContent> {
                                   color: Colors.black87,
                                   borderRadius: BorderRadius.vertical(top: Radius.circular(20.px)),
                                   image: DecorationImage(
+                                    alignment: Alignment(x, y),
                                     fit: BoxFit.cover,
                                       image:AssetImage(homeList[index].image)
                                   ),
@@ -119,80 +231,92 @@ class _HomeScreenContentState extends State<HomeScreenContent> {
                   initialPage: 0,
                   clipBehavior: Clip.none,
                 )
-            ),
+            ),*/
           ],
         ),
         Align(
           alignment: Alignment.bottomCenter,
           child: GestureDetector(
             onLongPress: () {
-              isDrag=!isDrag;
-              setState(() {
-
-              });
+              isDrag = !isDrag;
+              setState(() {});
             },
-            child:isDrag?Container(
-              width: double.infinity,
-              height:80.h,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(15.px)),
-                  color: Colors.orange
-              ),
-              child:Container(
-                width: double.infinity,
-                color: Colors.white12,
-                child: Column(children: [
-                  Expanded(
+            child: isDrag
+                ? Container(
+                    width: double.infinity,
+                    height: 80.h,
+                    decoration: BoxDecoration(
+                        borderRadius:
+                            BorderRadius.vertical(top: Radius.circular(15.px)),
+                        color: Colors.orange),
+                    child: Container(
+                      width: double.infinity,
+                      color: Colors.white12,
+                      child: Column(
+                        children: [
+                          Expanded(
+                            child: ListView.separated(
+                                padding: EdgeInsets.all(10.px),
+                                itemBuilder: (context, index) => Row(
+                                      children: [
+                                        ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(20.px),
+                                          child: Image(
+                                            height: 15.h,
+                                            width: 30.w,
+                                            fit: BoxFit.cover,
+                                            image: AssetImage(
+                                                homeList[index].image),
+                                          ),
+                                        ),
+                                        const Spacer(),
+                                        Text(
+                                          homeList[index].text,
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.w700,
+                                              fontSize: 20.px),
+                                        ),
+                                      ],
+                                    ),
+                                separatorBuilder: (context, index) => SizedBox(
+                                      height: 3.h,
+                                    ),
+                                itemCount: homeList.length),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                    .animate()
+                    .fadeIn(duration: 800.ms, curve: Curves.easeInOut)
+                    .show(
+                      duration: 100.ms,
+                    )
+                : Container(
+                    width: double.infinity,
+                    height: 10.h,
+                    decoration: BoxDecoration(
+                        borderRadius:
+                            BorderRadius.vertical(top: Radius.circular(15.px)),
+                        color: Colors.orange),
                     child: ListView.separated(
-                      padding: EdgeInsets.all(10.px),
-                        itemBuilder: (context, index) => Row(
-
-                          children: [
-                            ClipRRect(
+                        padding: EdgeInsets.all(10.px),
+                        physics: const NeverScrollableScrollPhysics(),
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) => ClipRRect(
                               borderRadius: BorderRadius.circular(20.px),
                               child: Image(
-                                height: 15.h,
-                                  width: 30.w,
+                                  width: 70.px,
                                   fit: BoxFit.cover,
-                                  image:AssetImage(homeList[index].image),
-                              ),
+                                  image: AssetImage(homeList[index].image)),
                             ),
-                            const Spacer(),
-                            Text(homeList[index].text,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w700,
-                              fontSize: 20.px
+                        separatorBuilder: (context, index) => SizedBox(
+                              width: 3.w,
                             ),
-                            ),
-                          ],
-                        ),
-                        separatorBuilder: (context, index) => SizedBox(height: 3.h,),
                         itemCount: homeList.length),
-                  )
-                ],),
-              ),
-            ).animate().fadeIn(duration: 800.ms,curve: Curves.easeInOut).show(duration: 100.ms,):Container(
-              width: double.infinity,
-              height:10.h,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(15.px)),
-                  color: Colors.orange
-              ),
-              child: ListView.separated(
-                padding: EdgeInsets.all(10.px),
-                  physics: const NeverScrollableScrollPhysics(),
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder:(context,index)=>ClipRRect(
-                    borderRadius: BorderRadius.circular(20.px),
-                    child: Image(
-                        width: 70.px,
-                        fit: BoxFit.cover,
-                        image: AssetImage(homeList[index].image)),
-                  ),
-                  separatorBuilder: (context, index) => SizedBox(width: 3.w,),
-                  itemCount: homeList.length),
-            ).animate().fade(duration: 500.ms),
+                  ).animate().fade(duration: 500.ms),
           ),
         ),
       ],
